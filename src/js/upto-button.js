218e0db5c7ee687c0c412d7a4a4834e588f38001
o <42962454+change-my-username@users.noreply.github.com>
2020-10-17 19:35:16 +0300

    Upto button

---
+++ src/js/upto-button.js
@@ -0,0 +1,20 @@
+; 
+(
+    function uptoButton() {
+        window.addEventListener('scroll', (e) => {
+            const uptoButton = document.querySelector('.upto-button')
+            uptoButton.classList.add('hide')
+
+            if(window.scrollY >= 0.9 * window.innerHeight) {
+                console.log(window.scrollY)
+                
+                uptoButton.classList.remove('hide')
+
+            }
+           
+           
+
+        })
+    }
+)();
+
