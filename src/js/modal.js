a007f27ec8414cbc315d9361a21c2098b0eacd03
OlhaPylyp <68662437+OlhaPylyp@users.noreply.github.com>
2020-10-17 20:21:54 +0300

    add JS for two different button

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,13 @@
 (() => {
-    const refs = {        
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        openModalBtn: document.querySelector("[data-modal-open-two]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtnOpen: document.querySelector('[data-modal-open-desktop]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtnOpen.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
