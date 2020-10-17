b726365832ab26e6b45a8bdcd48cdb1ae48a81af
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-10-17 22:21:15 +0300

    Добавил блокировку при вызове модалки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      openModalBtnOpen: document.querySelector('[data-modal-open-desktop]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtnOpen: document.querySelector('[data-modal-open-desktop]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.openModalBtnOpen.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnOpen.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
-})()
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
